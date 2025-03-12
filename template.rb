a0f027cdff44252d0fec33c804fcba7ae14c7ae9
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-12 15:54:05 -0700

    Use UUIDv7 primary keys by default

--- template.rb
+++ template.rb
@@ -8,5 +8,33 @@ if caller.empty?
 end
 
 after_bundle do
+  generate :migration, "enable_pgcrypto_extension", "--skip"
+  pgcrypto_migration_file = Dir.glob("db/migrate/*_enable_pgcrypto_extension.rb").first
+
+  insert_into_file pgcrypto_migration_file, after: "  def change\n" do
+    <<~RUBY.indent(4)
+      enable_extension :pgcrypto
+    RUBY
+  end
+
+  initializer "generators.rb", <<~RUBY
+    Rails.application.config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+  RUBY
+
+  inject_into_file "app/models/application_record.rb", before: "end\n" do
+    <<~RUBY.indent(2)
+
+      before_create :generate_uuid_v7
+
+      private
+
+      def generate_uuid_v7
+        self.id ||= SecureRandom.uuid_v7
+      end
+    RUBY
+  end
+
   rails_command "db:migrate"
 end
