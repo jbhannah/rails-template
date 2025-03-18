4b94ac9efea883185d895ef5e69786a497828a7c
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-17 20:37:11 -0700

    Clean up bootstrap file

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,109 +1,117 @@
 #!/usr/bin/env ruby
 
-options = {
-  css: "postcss",
-  database: "postgresql",
-  devcontainer: true,
-  javascript: "bun",
-}
+require "pathname"
 
-def install_rails
-  gemfile = ['gem "rails"']
+def options
+  {
+    css: "postcss",
+    database: "postgresql",
+    devcontainer: true,
+    javascript: "bun",
+  }
+end
 
-  if not File.exist?("Gemfile")
-    gemfile.unshift('source "https://rubygems.org"')
-    created_gemfile = true
-  elsif File.readlines("Gemfile").any?(/gem "rails"/)
+def bundle_rails(dir)
+  gemfile = dir / "Gemfile"
+
+  if gemfile.exist? and gemfile.read.include?('gem "rails"')
     puts "Gemfile already contains rails, skipping"
-    skipped_gemfile = true
+    return
+  elsif gemfile.empty? or not gemfile.exist?
+    gemfile.write('source "https://rubygems.org"')
+    force = true
   end
 
-  puts <<~EOF
-
-    Writing the following to Gemfile and running bundle install:
-
-    #{gemfile.map { |line| "    " + line }.join("\n")}
-
-  EOF
-
-  File.open("Gemfile", "a") do |f|
-    f.write gemfile.join("\n")
-  end unless skipped_gemfile
-
+  gemfile.write('gem "rails"')
   system(*%w[bundle install])
 
-  return created_gemfile
+  return force
 end
 
-if (%w[-h --help] & ARGV).any?
-  require "tmpdir"
+def help
+  if (%w[-h --help] & ARGV).any?
+    Pathname.mktmpdir do |dir|
+      bundle_rails(dir)
 
-  Dir.mktmpdir do |dir|
-    Dir.chdir(dir) do
-      install_rails
-      system(*%w[bundle exec rails new --help])
+      Dir.chdir(dir) do
+        system(*%w[bundle exec rails new --help])
+      end
     end
+
+    exit 0
   end
-
-  exit 0
 end
 
-if ARGV.empty? or ARGV[0].start_with?("-")
-  puts "An app path or -h/--help is required as the first argument"
-  exit 1
-end
-
-app_path = ARGV.shift
-
-if app_path != "."
-  if File.exist?(app_path) and not (Dir.empty?(app_path) or (%w[-f --force] & ARGV).any?)
-    puts "#{app_path} already exists and is not an empty directory, aborting"
+def create_app_path
+  if ARGV.empty? or ARGV[0].start_with?("-")
+    puts "An app path or -h/--help is required as the first argument"
     exit 1
   end
 
-  Dir.mkdir(app_path) unless Dir.exist?(app_path)
-  Dir.chdir(app_path)
+  app_path_arg = ARGV.shift
+  is_cwd = app_path_arg == "."
+
+  app_path = Pathname.new(app_path_arg)
+
+  unless is_cwd
+    if app_path.exist? and not ((app_path.directory? and app_path.empty?) or (%w[-f --force] & ARGV).any?)
+      puts "#{app_path} already exists and is not an empty directory, aborting"
+      exit 1
+    end
+
+    app_path.mkdir unless app_path.exist?
+  end
+
+  return app_path, is_cwd
 end
 
-File.open(".ruby-version", "w") do |f|
-  f.write RUBY_VERSION
-end unless File.exist?(".ruby-version")
+def set_ruby_version(dir)
+  ruby_version_file = dir / Pathname(".ruby-version")
+  ruby_version_file.write(RUBY_VERSION) unless ruby_version_file.exist?
 
-system("rbenv install --skip-existing")
-
-created_gemfile = install_rails
-
-rails_new_command = %w[bundle exec rails new .]
-rails_new_command << "--force" if created_gemfile
-
-options.each do |key, value|
-  rails_new_command << "--#{key}=#{value}"
+  system("rbenv install --skip-existing #{RUBY_VERSION}")
 end
 
-rails_new_command += ARGV
+def rails_new(dir, force, is_cwd)
+  rails_new_command = %w[bundle exec rails new .]
+  rails_new_command << "--force" if force
 
-if File.exist?("../template.rb")
-  rails_new_command << "--template=../template.rb"
-else
-  rails_new_command << "--template=https://raw.githubusercontent.com/jbhannah/rails-template/refs/heads/trunk/template.rb"
-end
+  options.each do |key, value|
+    rails_new_command << "--#{key}=#{value}"
+  end
 
-puts <<~EOF
+  rails_new_command += ARGV
 
-  Creating Rails app in #{app_path} with the following command:
+  if File.exist?("../template.rb")
+    rails_new_command << "--template=../template.rb"
+  else
+    rails_new_command << "--template=https://raw.githubusercontent.com/jbhannah/rails-template/refs/heads/trunk/template.rb"
+  end
 
-      #{rails_new_command.join(" ")}
-
-EOF
-
-if system(*rails_new_command)
   puts <<~EOF
 
-    Rails app created successfully#{" in #{app_path}" if app_path != "."}!
-    You can now#{" cd into it and" if app_path != "."} start it with:
+    Creating Rails app in #{dir} with the following command:
 
-        #{"cd #{app_path}\n    " if app_path != "."}bin/dev
+        #{rails_new_command.join(" ")}
+
   EOF
-else
-  exit $?.exitstatus
+
+  Dir.chdir(dir) do
+    if system(*rails_new_command)
+      puts <<~EOF
+
+        Rails app created successfully#{" in #{dir}" unless is_cwd}!
+        You can now#{" cd into it and" unless is_cwd} start it with:
+
+            #{"cd #{dir}\n    " unless is_cwd}bin/dev
+      EOF
+    else
+      exit $?.exitstatus
+    end
+  end
 end
+
+help
+app_path, is_cwd = create_app_path
+created_gemfile = bundle_rails(app_path)
+rails_new(app_path, created_gemfile, is_cwd)
